3d87283872684dc093783c5d442f8c02240010f9
Ryan XVX <ryanxmccarthy@gmail.com>
2020-07-22 09:54:03 -0700

    added about page to router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import PageWrapper from './components/PageWrapper';
-import Home from './components/pages/Home'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+// Pages
+import Home from './components/pages/Home';
+import About from './components/pages/About';
 
 function App() {
   return (
     <Router>
       <PageWrapper>
         <Route 
+          exact={true}
           path='/'
           component={ Home }
+        />
+
+        <Route 
+          path='/about'
+          component={ About }
         />
       </PageWrapper>
     </Router>
