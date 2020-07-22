9584903270b7f59b05cf8e337d21b9f210cfc6c3
Ryan XVX <ryanxmccarthy@gmail.com>
2020-07-22 13:37:12 -0700

    beginning portfolio

--- src/components/common/Services.js
+++ src/components/common/Services.js
@@ -10,13 +10,13 @@ const services = [
 class Services extends Component {
     render() {
         return (
-            <section class="page-section" id="services">
-                <div class="container">
-                    <div class="text-center">
-                        <h2 class="section-heading text-uppercase">Services</h2>
-                        <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+            <section className="page-section" id="services">
+                <div className="container">
+                    <div className="text-center">
+                        <h2 className="section-heading text-uppercase">Services</h2>
+                        <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
                     </div>
-                    <div class="row text-center">
+                    <div className="row text-center">
                         {services.map((service, index) => {
                             return <SingleService {...service} key={index} />
                         })}
