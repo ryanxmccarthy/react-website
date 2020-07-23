088dc9b068c7f7346756aadc154a3b3d6620e8d7
Ryan XVX <ryanxmccarthy@gmail.com>
2020-07-23 11:28:39 -0700

    added low level form functionality

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,4 +1,18 @@
 import React, {Component} from 'react';
+import Field from '../common/Field';
+
+const fields = {
+    sections: [
+        [
+            {name: 'name', elementName: 'input', type: 'text', placeholder: 'Your name'},
+            {name: 'email', elementName: 'input', type: 'email', placeholder: 'Your email'},
+            {name: 'phone', elementName: 'input', type: 'text', placeholder: 'Your phone number'},
+        ],
+        [
+            {name: 'message', elementName: 'textarea', type: 'text', placeholder: 'Your message'}
+        ]
+    ]
+}
 
 class Contact extends Component {
     constructor(props) {
@@ -11,6 +25,10 @@ class Contact extends Component {
         }
     }
 
+    submitForm = (e) => {
+        alert('Form submitted, thank you!');
+    }
+
     render() {
         return (
             <section className="page-section" id="contact">
@@ -19,67 +37,36 @@ class Contact extends Component {
                         <h2 className="section-heading text-uppercase">Contact Us</h2>
                         <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
                     </div>
-                    <form id="contactForm" name="sentMessage" novalidate="novalidate">
+                    <form 
+                        id="contactForm" 
+                        name="sentMessage" novalidate="novalidate"
+                    >
                         <div className="row align-items-stretch mb-5">
-                            <div className="col-md-6">
-                                <div className="form-group">
-                                    <input 
-                                        className="form-control" 
-                                        id="name" 
-                                        type="text" 
-                                        placeholder="Your Name *" 
-                                        required="required" 
-                                        data-validation-required-message="Please enter your name." 
-                                        value={this.state.name}
-                                        onChange={e => this.setState({name: e.target.value})}
-                                    />
-                                    <p className="help-block text-danger"></p>
-                                </div>
-                                <div className="form-group">
-                                    <input 
-                                        className="form-control" 
-                                        id="email" 
-                                        type="email" 
-                                        placeholder="Your Email *" 
-                                        required="required" 
-                                        data-validation-required-message="Please enter your email address." 
-                                        value={this.state.email}
-                                        onChange={e => this.setState({email: e.target.value})}
-                                    />
-                                    <p className="help-block text-danger"></p>
-                                </div>
-                                <div className="form-group mb-md-0">
-                                    <input 
-                                        className="form-control" 
-                                        id="phone" 
-                                        type="tel" 
-                                        placeholder="Your Phone *" 
-                                        required="required" 
-                                        data-validation-required-message="Please enter your phone number." 
-                                        value={this.state.phone}
-                                        onChange={e => this.setState({phone: e.target.value})}
-                                    />
-                                    <p className="help-block text-danger"></p>
-                                </div>
-                            </div>
-                            <div className="col-md-6">
-                                <div className="form-group form-group-textarea mb-md-0">
-                                    <textarea 
-                                        className="form-control" 
-                                        id="message" 
-                                        placeholder="Your Message *" 
-                                        required="required" 
-                                        data-validation-required-message="Please enter a message."
-                                        value={this.state.message}
-                                        onChange={e => this.setState({message: e.target.value})}
-                                    />
-                                    <p className="help-block text-danger"></p>
-                                </div>
-                            </div>
+                            {fields.sections.map((section, sectionIndex) => {
+                                return (
+                                    <div className='col-md-6' key={sectionIndex}>
+                                        {section.map((field, i) => {
+                                            return <Field 
+                                                        {...field} 
+                                                        key={i} 
+                                                        value={this.state[field.name]}
+                                                        onChange={e => this.setState({
+                                                            [field.name]: e.target.value
+                                                        })}
+                                                    />
+                                        })}
+                                    </div>
+                                )
+                            })}
                         </div>
                         <div className="text-center">
                             <div id="success"></div>
-                            <button className="btn btn-primary btn-xl text-uppercase" id="sendMessageButton" type="submit">Send Message</button>
+                            <button 
+                                className="btn btn-primary btn-xl text-uppercase" 
+                                id="sendMessageButton" 
+                                type="submit"
+                                onClick={e => this.submitForm(e)}
+                            >Send Message</button>
                         </div>
                     </form>
                 </div>
