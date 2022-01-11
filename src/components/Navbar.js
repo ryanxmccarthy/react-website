21f83c6d8308beea65a18899c77b65872c7e288c
Ryan XVX <ryanxmccarthy@gmail.com>
2022-01-11 14:42:22 -0800

    handle click for hamburger menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,9 @@ import { Link } from 'react-router-dom'
 
 function Navbar() {
     const [click, setClick] = useState(false)
-    
+
+    const handleClick = () => setClick(!click);
+
     return (
         <>
             <nav className='navbar'>
@@ -11,7 +13,7 @@ function Navbar() {
                     <Link to="/" className='navbar-logo'>
                         RMM <i className='fab fa-typo3'></i>
                     </Link>
-                    <div className='menu-icon'>
+                    <div className='menu-icon' onClick={handleClick}>
                         <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
                     </div>
                 </div>
