4fe7aaf08527c0bb10444fee7d9ca2d5238934d2
Ryan XVX <ryanxmccarthy@gmail.com>
2022-01-13 09:58:03 -0800

    dynamically creating buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -8,4 +8,14 @@ const SIZES = ['btn--medium', 'btn-large']
 
 export const Button = ({children, type, onClick, buttonStyle, buttonSize}) => {
     const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0]
+
+    const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0]
+
+    return (
+        <Link to='/sign-up' className='btn-mobile'>
+            <button>
+                {children}
+            </button>
+        </Link>
+    )
 }
