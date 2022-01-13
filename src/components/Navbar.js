416d63c843e504f435aa3590b586a11176d0bf8a
Ryan XVX <ryanxmccarthy@gmail.com>
2022-01-12 16:22:32 -0800

    adding rest of menu items

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -22,6 +22,15 @@ function Navbar() {
                         <li className='nav-item'>
                             <Link to='/' className='nav-links' onClick={closeMobileMenu}>Home</Link>
                         </li>
+                        <li className='nav-item'>
+                            <Link to='/services' className='nav-links' onClick={closeMobileMenu}>Services</Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/products' className='nav-links' onClick={closeMobileMenu}>Products</Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>Sign Up</Link>
+                        </li>
                     </ul>
                 </div>
             </nav>
