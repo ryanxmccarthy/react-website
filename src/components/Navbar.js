d470ce31012dace53cb921e5622a3d3362b3ea6c
Ryan XVX <ryanxmccarthy@gmail.com>
2022-01-14 10:04:53 -0800

    working on importing and exporting buttons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,10 +3,20 @@ import { Link } from 'react-router-dom'
 
 function Navbar() {
     const [click, setClick] = useState(false)
+    const [button, setButton] = useState(true);
 
     const handleClick = () => setClick(!click);
-
     const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if(window.innerWidth <= 960) {
+            setButton(false)
+        } else {
+            setButton(true)
+        }
+    }
+
+    window.addEventListener('resize', showButton);
 
     return (
         <>
@@ -32,6 +42,7 @@ function Navbar() {
                             <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>Sign Up</Link>
                         </li>
                     </ul>
+                    {/* {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>} */}
                 </div>
             </nav>
         </>
