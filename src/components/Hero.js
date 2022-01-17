41ad2adcf826b75103089d6e8b052a53d01081a4
Ryan XVX <ryanxmccarthy@gmail.com>
2022-01-17 09:50:37 -0800

    adding button to hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Button } from './Button'
 
 function Hero() {
     return (
@@ -6,6 +7,13 @@ function Hero() {
             <video src='/videos/video-2.mp4' autoPlay loop muted />
             <h1>ADVENTURE AWAITS</h1>
             <p>What are you waiting for?</p>
+            <div classname='hero-btns'>
+                <Button 
+                    className='btns' 
+                    buttonStyle='btn--outline'
+                    buttonSize='btn--large'>
+                GET STARTED</Button>
+            </div>
         </div>
     )
 }
