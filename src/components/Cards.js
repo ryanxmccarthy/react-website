be04af8cb4a69c3a572bd08f3710af519ec7a555
Ryan XVX <ryanxmccarthy@gmail.com>
2022-01-21 09:56:02 -0800

    adding cards to home page

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import Card from './Card'
 
 function Cards() {
   return <div className='cards'>
       <h1>Check out these epic destinations</h1>
+      <div className='cards__container'>
+        <div className='cards__wrapper'>
+          <ul className='cards__items'>
+            <Card />
+          </ul>
+        </div>
+      </div>
   </div>;
 }
 
