761e162424bb70213b81542b7e1c6ceaabc9d283
Ryan XVX <ryanxmccarthy@gmail.com>
2022-01-21 09:59:40 -0800

    cards are breaking the page so thats cool

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,14 +7,14 @@ function Card() {
         <li className='cards__item'>
             <Link className='cards__item__link'>
                 <figure className='cards__item__pic-wrap'>
-                     <img src='/' alt='Travel Image' className='cards__item__img' />
+                     <img src='/' alt='Travel' className='cards__item__img' />
                 </figure>
                 <div className='cards__item__info'>
-                    <h5 className='cards__item__text' />
+                    <h5 className='cards__item__text'>Test</h5>
                 </div>
             </Link>
         </li>
-    </>;
+    </>
   )
 }
 
