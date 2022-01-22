df6a8579c0042b304e7b57a2fcce27b47dea3a24
Ryan XVX <ryanxmccarthy@gmail.com>
2022-01-22 09:48:33 -0800

    adding second card

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -10,8 +10,14 @@ function Cards() {
           <ul className='cards__items'>
             <Card 
               src='images/img-9.jpg'
-              text='Explore the hidden watterfall deep inside the Amazon jungle'
+              text='Explore the hidden waterfall deep inside the Amazon jungle'
               label='Adventure'
+              path='/services'
+            />
+            <Card 
+              src='images/img-2.jpg'
+              text='Travel through the Islands of Bali in a Private Cruise'
+              label='Luxury'
               path='/services'
             />
           </ul>
