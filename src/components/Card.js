482af2cee02ac5760ca0e8e8850dee70da45f338
Ryan XVX <ryanxmccarthy@gmail.com>
2022-01-22 09:45:52 -0800

    basic card styling done

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,11 +6,11 @@ function Card(props) {
     <>
         <li className='cards__item'>
             <Link to='/' className='cards__item__link' to={props.path}>
-                <figure className='cards__item__pic-wrap'>
-                     <img src='/' alt='Travel' className='cards__item__img' />
+                <figure className='cards__item__pic-wrap' data-category={props.label}>
+                     <img src={props.src} alt='Travel' className='cards__item__img' />
                 </figure>
                 <div className='cards__item__info'>
-                    <h5 className='cards__item__text'>Test</h5>
+                    <h5 className='cards__item__text'>{props.text}</h5>
                 </div>
             </Link>
         </li>
