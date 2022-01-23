3ebba6ac154adff0062fa68f882937410b7ea64f
Ryan XVX <ryanxmccarthy@gmail.com>
2022-01-23 11:33:41 -0800

    adding new line of cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -21,6 +21,26 @@ function Cards() {
               path='/services'
             />
           </ul>
+          <ul className='cards__items'>
+            <Card 
+              src='images/img-9.jpg'
+              text='Explore the hidden waterfall deep inside the Amazon jungle'
+              label='Adventure'
+              path='/services'
+            />
+            <Card 
+              src='images/img-2.jpg'
+              text='Travel through the Islands of Bali in a Private Cruise'
+              label='Luxury'
+              path='/services'
+            />
+            <Card 
+              src='images/img-2.jpg'
+              text='Travel through the Islands of Bali in a Private Cruise'
+              label='Luxury'
+              path='/services'
+            />
+          </ul>
         </div>
       </div>
   </div>;
