f00a5734cdfb6c02cf62134da7e8ad88bfa7f945
Ryan XVX <ryanxmccarthy@gmail.com>
2022-01-24 11:49:09 -0800

    paths created, need to figure out new routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import './App.css';
-import Home from './components/pages/Home';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
@@ -12,6 +15,9 @@ function App() {
         <Home />
         <Routes>
           <Route path='/' exact component={Home} />
+          <Route path='/services' component={Services} />
+          <Route path='/products' component={Products} />
+          <Route path='/sign-up' component={SignUp} />
         </Routes>
       </Router>
     </>
