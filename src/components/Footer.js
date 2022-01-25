951bc0cc945e217668973670d6b320044469ba83
Ryan XVX <ryanxmccarthy@gmail.com>
2022-01-25 09:50:26 -0800

    creating footer

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Footer() {
+  return <div className='footer-container'>
+      <section className='footer-subscription'></section>
+  </div>;
+}
+
+export default Footer;
