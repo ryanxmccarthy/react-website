e775a39a89c5f7a2541991c81b796ceca19cbd55
Ryan XVX <ryanxmccarthy@gmail.com>
2022-01-25 09:52:20 -0800

    footer outline created

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 function Footer() {
   return <div className='footer-container'>
-      <section className='footer-subscription'></section>
+      <section className='footer-subscription'>
+          <p className='footer-subscription-heading'>Join the Adventure newsletter to receive our best vacation deals.</p>
+          <p className='footer-subscription-text'>You can unsubscribe at any time.</p>
+          <div className='input-areas'>
+              <form>
+                  <input type='email' name='email' placeholder='Your Email' className='footer-input' />
+              </form>
+          </div>
+      </section>
   </div>;
 }
 
